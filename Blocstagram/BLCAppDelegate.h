fe8c4ac8170d6ba0592f1a56985557e8ded023f9
Stephen Murphy <smurphylaw@gmail.com>
2014-08-20 00:08:13 -0600

    Initial Commit

---
+++ Blocstagram/BLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLCAppDelegate.h
+//  Blocstagram
+//
+//  Created by Murphy on 8/20/14.
+//  Copyright (c) 2014 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Blocstagram/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by Murphy on 8/20/14.
+//  Copyright (c) 2014 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BLCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BLCAppDelegate class]));
+    }
+}
