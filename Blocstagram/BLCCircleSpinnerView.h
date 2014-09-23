86d9f0e87fe06ea0b1942aa56388b8e169c896f8
Stephen Murphy <smurphylaw@gmail.com>
2014-09-23 13:27:01 -0600

    Fixed for 32 back button

---
+++ Blocstagram/BLCCircleSpinnerView.h
@@ -0,0 +1,17 @@
+//
+//  BLCCircleSpinnerView.h
+//  Blocstagram
+//
+//  Created by Murphy on 9/21/14.
+//  Copyright (c) 2014 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCCircleSpinnerView : UIView
+
+@property (nonatomic, assign) CGFloat strokeThickness;
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, strong) UIColor *strokeColor;
+
+@end
